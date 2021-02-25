7cbbf323341dc976f9188f2667924575e280e5fb
HuhuliaDiana <dianahuhulia@gmail.com>
2021-02-25 19:07:55 +0200

    third commit

---
+++ Proiect_sem1/src/clase/Zebra.java
@@ -0,0 +1,20 @@
+package clase;
+
+public class Zebra extends Animal{
+	private int varsta;
+	
+
+	public Zebra(String name) {
+		super(name);
+	}
+
+	public int getVarsta() {
+		return varsta;
+	}
+
+	public void setVarsta(int varsta) {
+		this.varsta = varsta;
+	}
+	
+
+}
